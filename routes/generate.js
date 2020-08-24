c2f25e4c3b682e99c7dc90106344dca07680288b
Luiz Peres <luiz_peres10@hotmail.com>
2020-08-23 23:27:07 -0700

    Creating the smallest possible example

--- routes/generate.js
+++ routes/generate.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const d3node = require('d3-node')
 var router = express.Router();
 
 /* GET generate listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const d3n = new d3node()
+  d3n.createSVG(500,500)
+  .append("rect")
+  .attr("width", "100%")
+  .attr("height", "100%")
+  .attr("fill", "pink");
+  res.send(d3n.svgString());
 });
 
 module.exports = router;
